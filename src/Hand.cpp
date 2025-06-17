bf394a2580c754204e9b2eeb77812c1510015f44
gablabz <gabriel.labbe.3@ulaval.ca>
2025-06-17 08:18:43 -0400

    Hand::card1 > Hand::card2

--- src/Hand.cpp
+++ src/Hand.cpp
@@ -1,7 +1,11 @@
 #include "../include/Hand.h"
+#include <algorithm>
 
-Hand::Hand(Card c1, Card c2) : card1(c1), card2(c2) {
-    mask = (1ULL << c1.getId()) | (1ULL << c2.getId());
+Hand::Hand(Card c1, Card c2) : card1(c1), card2(c2), mask(0) {
+    if (static_cast<int>(card1.getRank()) < static_cast<int>(card2.getRank())) {
+        std::swap(card1, card2);
+    }
+    mask = (1ULL << card1.getId()) | (1ULL << card2.getId());
 }
 
 Card Hand::getCard1() const {
